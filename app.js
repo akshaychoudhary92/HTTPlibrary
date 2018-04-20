837de6c08901e56056a8ebeb77e959f5fd5e1a46
Akshay Choudhary <ak92choudhary@gmail.com>
2018-04-20 19:07:18 -0400

    upgraded to async await

--- app.js
+++ app.js
@@ -1,60 +1,28 @@
-const http = new easyHTTP;
+const http = new EasyHTTP;
 
-// GET posts
+// Get Users
+// http.get('https://jsonplaceholder.typicode.com/users')
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
 
-http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
-    if(err) {
-        console.log(err);
-    }else {
-        console.log(posts)
-    }
-    console.log(posts);
-});
-
-// get single post
-http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, post) {
-    if(err) {
-        console.log(err);
-    }else {
-        console.log(post)
-    }
-
-});
-
-// create the data
-
+// User Data
 const data = {
-    title: 'Custom Post',
-    body: 'this is a custom post'
+  name: 'John Doe',
+  username: 'johndoe',
+  email: 'jdoe@gmail.com'
+}
 
-};
+// Create User
+// http.post('https://jsonplaceholder.typicode.com/users', data)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
 
-//create post
-http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post){
-    if(err) {
-                console.log(err);
-            }else {
-                console.log(post)
-            }
-})
+// Update Post
+// http.put('https://jsonplaceholder.typicode.com/users/2', data)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
 
-
-// update post
-http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post){
-    if(err) {
-                console.log(err);
-            }else {
-                console.log(post)
-            }
-})
-
-//delete post
-
-http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
-    if(err) {
-        console.log(err);
-    }else {
-        console.log(response)
-    }
-
-});
+// Delete User
+http.delete('https://jsonplaceholder.typicode.com/users/2')
+.then(data => console.log(data))
+.catch(err => console.log(err));
